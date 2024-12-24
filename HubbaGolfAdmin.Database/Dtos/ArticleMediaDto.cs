830c09cee4007d6c330ae79a7b853b1d3981b90e
Ngô Phi Lít <litngo@pacvn.vn>
2024-12-24 10:30:06 +0700

    no message

---
+++ HubbaGolfAdmin.Database/Dtos/ArticleMediaDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HubbaGolfAdmin.Database.Dtos
+{
+    public class ArticleMediaDto
+    {
+        public int? ArticleId { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Url { get; set; }
+
+        public int? Type { get; set; }
+    }
+}
